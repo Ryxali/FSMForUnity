8e89de0f52c5774cfa5bba7308e315f89abc49a9
Niklas Lindblad <ryxali@gmail.com>
2023-12-15 18:24:51 +0100

    Trigger Dispatch idea

---
+++ Packages/com.ryxali.fsmforunity/Runtime/Transitions/TriggerDispatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace FSMForUnity
+{
+    internal class TriggerDispatch
+    {
+        public delegate void OnTrigger(uint id);
+
+        public event OnTrigger onTrigger = delegate { };
+
+        private bool isStalled;
+        private readonly List<uint> backlog = new List<uint>();
+
+        public void Trigger(uint id)
+        {
+            if (isStalled)
+                backlog.Add(id);
+            else
+                onTrigger(id);
+        }
+
+        public void Stall()
+        {
+            isStalled = true;
+        }
+
+        public void Resume()
+        {
+            for (int i = 0; i < backlog.Count; i++)
+            {
+                onTrigger(backlog[i]);
+            }
+            backlog.Clear();
+            isStalled = false;
+        }
+    }
+}
---
+++ Packages/com.ryxali.fsmforunity/Runtime/TriggeredTransition.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FSMForUnity
+{
+    public struct TriggeredTransition
+    {
+        private readonly TriggerDispatch dispatch;
+        internal readonly uint id;
+
+        internal TriggeredTransition(TriggerDispatch dispatch, uint id)
+        {
+            this.dispatch = dispatch;
+            this.id = id;
+        }
+
+        public void Trigger()
+        {
+            dispatch.Trigger(id);
+        }
+    }
+}
