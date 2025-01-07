98b78a1d7205860d82e03a63d287edfac6c6f7e2
Niklas Lindblad <ryxali@gmail.com>
2025-01-07 09:31:54 +0100

    Add Drawer for TriggeredFSMTransition
    This way it can be fired in inspector

---
+++ Packages/com.ryxali.fsmforunity/Drawers/TriggeredFSMTransitionDrawer.cs
@@ -0,0 +1,85 @@
+using FSMForUnity;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CustomPropertyDrawer(typeof(TriggeredFSMTransition))]
+public class TriggeredFSMTransitionDrawer : PropertyDrawer
+{
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        var container = new VisualElement();
+
+        var obj = GetTargetObjectOfProperty(property);
+        var type = obj.GetType();
+        var method = type.GetMethod(nameof(TriggeredFSMTransition.Trigger));
+        
+        var button = new Button();
+        button.text = property.displayName;
+        button.clicked += () => method.Invoke(obj, null);
+        container.Add(button);
+
+        return container;
+    }
+
+    private static object GetTargetObjectOfProperty(SerializedProperty prop)
+    {
+        if (prop == null) return null;
+
+        var path = prop.propertyPath.Replace(".Array.data[", "[");
+        object obj = prop.serializedObject.targetObject;
+        var elements = path.Split('.');
+        foreach (var element in elements)
+        {
+            if (element.Contains("["))
+            {
+                var elementName = element.Substring(0, element.IndexOf("["));
+                var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                obj = GetValue_Imp(obj, elementName, index);
+            }
+            else
+            {
+                obj = GetValue_Imp(obj, element);
+            }
+        }
+        return obj;
+    }
+
+    private static object GetValue_Imp(object source, string name)
+    {
+        if (source == null)
+            return null;
+        var type = source.GetType();
+
+        while (type != null)
+        {
+            var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (f != null)
+                return f.GetValue(source);
+
+            var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            if (p != null)
+                return p.GetValue(source, null);
+
+            type = type.BaseType;
+        }
+        return null;
+    }
+
+    private static object GetValue_Imp(object source, string name, int index)
+    {
+        var enumerable = GetValue_Imp(source, name) as System.Collections.IEnumerable;
+        if (enumerable == null) return null;
+        var enm = enumerable.GetEnumerator();
+        //while (index-- >= 0)
+        //    enm.MoveNext();
+        //return enm.Current;
+
+        for (int i = 0; i <= index; i++)
+        {
+            if (!enm.MoveNext()) return null;
+        }
+        return enm.Current;
+    }
+}
--- Packages/com.ryxali.fsmforunity/Runtime/Transitions/TriggeredFSMTransition.cs
+++ Packages/com.ryxali.fsmforunity/Runtime/Transitions/TriggeredFSMTransition.cs
@@ -1,10 +1,13 @@
-﻿namespace FSMForUnity
+﻿using UnityEngine;
+
+namespace FSMForUnity
 {
     /// <summary>
     /// A transition that can only be passed through once it has been triggered via
     /// <see cref="TriggeredFSMTransition.Trigger"/>. Once the transition has been passed through,
     /// it must be triggered again to allow subsequent passthroughs.
     /// </summary>
+    [System.Serializable]
 	public sealed class TriggeredFSMTransition : IFSMTransition
     {
         private bool isSet;
