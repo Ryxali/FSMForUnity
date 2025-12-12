9fd748fdd58086d18cea9606ecb26e0a2f51974a
Niklas Lindblad <ryxali@gmail.com>
2025-12-12 10:21:55 +0100

    documentation

--- Packages/com.ryxali.fsmforunity/Runtime/States/DeferredCoroutineFSMState.cs
+++ Packages/com.ryxali.fsmforunity/Runtime/States/DeferredCoroutineFSMState.cs
@@ -1,43 +1,81 @@
-﻿using FSMForUnity;
-using System.Collections;
-
-public class DeferredCoroutineFSMState : CoroutineFSMState
+﻿using System.Collections;
+namespace FSMForUnity
 {
-    private readonly System.Func<IEnumerator> enter;
-    private readonly System.Func<DeltaTime, IEnumerator> enter2;
-    private readonly System.Action exit;
+    /// <summary>
+    /// A state that executes the assigned coroutine method. The coroutine cannot be a lambda method (e.g. '() => ...').
+    /// See <see cref="CoroutineFSMState"/> for more about coroutine states.
+    /// </summary>
+    public class DeferredCoroutineFSMState : CoroutineFSMState
+    {
+        private readonly System.Func<IEnumerator> enterNoDelta;
+        private readonly System.Func<DeltaTime, IEnumerator> enter;
+        private readonly System.Action exit;
 
-    public DeferredCoroutineFSMState(System.Func<IEnumerator> enter, System.Action exit)
-    {
-        this.enter = enter;
-        this.exit = exit;
-    }
-    public DeferredCoroutineFSMState(System.Func<DeltaTime, IEnumerator> enter, System.Action exit)
-    {
-        this.enter2 = enter;
-        this.exit = exit;
-    }
+        /// <summary>
+        /// Construct the state, disregarding access to the state machine's <see cref="CoroutineFSMState.DeltaTime"/>.
+        /// </summary>
+        /// <param name="enter"></param>
+        /// <param name="exit"></param>
+        public DeferredCoroutineFSMState(System.Func<IEnumerator> enter, System.Action exit)
+        {
+            if (enter != null)
+                enterNoDelta = enter;
+            else
+                enterNoDelta = Empty;
 
-    protected override IEnumerator Enter(DeltaTime deltaTime)
-    {
-        if (enter != null)
-        {
-            return enter.Invoke();
+            this.enter = EnterNoDelta;
+
+            if (exit != null)
+                this.exit = exit;
+            else
+                exit = NoExit;
         }
-        else if (enter2 != null)
+
+        /// <summary>
+        /// Construct the state, targeting a coroutine with <see cref="CoroutineFSMState.DeltaTime"/> as a parameter.
+        /// </summary>
+        /// <param name="enter"></param>
+        /// <param name="exit"></param>
+        public DeferredCoroutineFSMState(System.Func<DeltaTime, IEnumerator> enter, System.Action exit)
         {
-            return enter2.Invoke(deltaTime);
+            if (enter != null)
+            {
+                this.enter = enter;
+            }
+            else
+            {
+                this.enter = EnterNoDelta;
+                enterNoDelta = Empty;
+            }
+
+            if (exit != null)
+                this.exit = exit;
+            else
+                exit = NoExit;
         }
-        else
+
+        protected override IEnumerator Enter(DeltaTime deltaTime)
+        {
+            return enter.Invoke(deltaTime);
+        }
+
+        private IEnumerator EnterNoDelta(DeltaTime deltaTime)
+        {
+            return enterNoDelta();
+        }
+
+        private IEnumerator Empty()
         {
             return null;
         }
-    }
 
-    protected override void Exit()
-    {
-        exit?.Invoke();
-    }
+        protected override void Exit()
+        {
+            exit?.Invoke();
+        }
 
-    
+        private void NoExit() { }
+
+
+    }
 }
